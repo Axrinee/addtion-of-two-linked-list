fdbbef1b085833f4d18a479e30343aa908d7ddff
Axrinee <harinishree085@gmail.com>
2025-04-01 18:55:23 +0530

    LinkedListAddition.java

--- LinkedListAddition.java
+++ LinkedListAddition.java
@@ -8,26 +8,26 @@ public class LinkedListAddition {
     ListNode dummyHead = new ListNode(0);
     ListNode current = dummyHead;
     int carry = 0;
-while (l1 != null || l2 != null || carry != 0) {
+while (l1!=null||l2!=null||carry!=0) {
             int sum = carry;
-            if (l1 != null) {
-                sum += l1.val;
-                l1 = l1.next;
+            if (l1!=null) {
+                sum+=l1.val;
+                l1=l1.next;
             }
-            if (l2 != null) {
-                sum += l2.val;
-                l2 = l2.next;
+            if (l2!=null) {
+                sum+=l2.val;
+                l2=l2.next;
             }
-            carry = sum / 10;
-            current.next = new ListNode(sum % 10);
-            current = current.next;
+            carry=sum/10;
+            current.next=new ListNode(sum%10);
+            current=current.next;
         }
         return dummyHead.next;
     }
 public static void printList(ListNode node) {
-while (node != null) {
+while (node!=null) {
  System.out.print(node.val + " ");
- node = node.next;
+ node=node.next;
       }
         System.out.println();
     }
